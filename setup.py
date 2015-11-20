5902f2cae2ee7952bd4a7b0eb9969e8f2f94438f
Jeremy Osborne <josborne@auction.com>
2015-11-19 20:26:53 -0800

    0.1.1
    mainly testing pip and how it works with github.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="module-doc",
-    version="0.1.0",
+    version="0.1.1",
     license="UNLICENSED",
     description="Simple module docstring to accessible doc files.",
     long_description=open("README.md").read(),
